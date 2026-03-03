4f4982ed9f7343f614a00dbc984af42cdf2b4b7b
Arpita Singh <arpitasingh212006@gmail.com>
2026-03-03 23:12:34 +0530

    Implemented Process class and basic process simulation

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from process import Process
+
+# Create sample processes
+p1 = Process("P1", 10, 200, 1)
+p2 = Process("P2", 15, 300, 2)
+
+# Display information
+p1.display_info()
+p2.display_info()
